8c4940d31ec087cf3b5c22bfe4eac4b19e4376c6
Christian Magnerfelt <christian.magnerfelt@gmail.com>
2012-08-21 22:39:50 +0200

    Added client directconnect functionality

--- Assets/Scripts/ClientBackend.cs
+++ Assets/Scripts/ClientBackend.cs
@@ -1,15 +1,42 @@
 using UnityEngine;
 using System.Collections;
 
-public class ClientBackend : MonoBehaviour {
+public class ClientBackend : MonoBehaviour
+{
+    public int      targetFrameRate     = 30;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public bool     directConnect       = false;
+    public string   directConnectIP     = "127.0.0.1";
+    public int      directConnectPort   = 8001;
+
+    void Awake()
+    {
+        Application.targetFrameRate = targetFrameRate;
+        uLink.Network.isAuthoritativeServer = true;
+
+    }
+
+    void Update()
+    {
+        if(directConnect && uLink.Network.status == uLink.NetworkStatus.Disconnected)
+        {
+            uLink.Network.Connect(directConnectIP, directConnectPort);
+            Debug.Log("Connecting to server at ip : " + directConnectIP + " on port " + directConnectPort);
+        }
+
+    }
+    private void uLink_OnConnectedToServer()
+    {
+        Debug.Log("Connected to server");
+    }
+
+    private void uLink_OnDisconnectedFromServer()
+    {
+        Debug.Log("Disconnected from server");
+    }
+
+    private void uLink_OnFailedToConnect()
+    {
+        Debug.Log("Failed to connect");
+    }
 }
